083f3cba606c4c50649e991e54b85ec73a5ff818
Keshav Varadarajan <keshav.varadarajan@duke.edu>
2024-03-29 20:47:17 -0400

    Initial Commit

---
+++ CampusETA/CampusETAApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusETAApp.swift
+//  CampusETA
+//
+//  Created by Keshav Varadarajan on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct CampusETAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
